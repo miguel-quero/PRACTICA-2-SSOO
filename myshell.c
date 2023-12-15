272397e27acd4ade44687707351c5f572ca63bf2
quer0 <m.quero.2021@alumnos.urjc.es>
2023-12-15 19:22:17 +0100

    cd comentado, exit hecho y comentado

--- myshell.c
+++ myshell.c
@@ -12,8 +12,9 @@
 #include <fcntl.h>
 //gcc -Wextra -Wall myshell.c -o myshell libparser.a -static
 
+int fin = 0; // Para finalizar el bucle del main
 
-int verificarmandato(char* mandato){
+int verificarmandato(char* mandato){ // Comprueba que el mandato se encuentra en la minishell
 	if(mandato != NULL){
 		return 0;
 	}else{
@@ -21,50 +22,49 @@ int verificarmandato(char* mandato){
 	}
 }
 
-void cd(tline * line){
-	char *dir; // Variable de directorios 
-	char buffer[512];
-	
-	if(line->commands[0].argc > 2) // No puedo hacer un cd a 2 directorios 
-		{
-		  fprintf(stderr,"Uso: %s directorio\n", line->commands[0].argv[0]);
-		}
-	if (line->commands[0].argc == 1) // Si vale 1 , no me pasan nada, osea nombre del programa.
-	{
-		dir = getenv("HOME");
-		if(dir == NULL)
-		{
-			fprintf(stderr,"No existe la variable $HOME\n");	
-		}
-	}else {
-		dir = line->commands[0].argv[1];
+void cd(tline * line){ //Se ejecuta cuando se elige el mandato cd
+	char *dir; //esta variable almacena el directorio elegido
+	char ruta[1024]; //almacena la ruta del directorio actual
+
+	if (line->commands[0].argc==1){ // Si el usuario no pone nada, el directorio es $HOME
+		dir=getenv("HOME");
+	}else { //Si el usuario incluye una ruta de directorio, se almacena en dir
+		dir=line->commands[0].argv[1];
 	}
-	
-	// Comprobar si es un directorio.
-	if (chdir(dir) != 0) { // Sino es distinto de 0 lo hace normal el chdir 
-		fprintf(stderr,"Error al cambiar de directorio: %s\n", strerror(errno)); // Los errores a llamada al sistema siempre se guardan en errno, y strerror explica el porque de errno.
-	} else {
-	printf( "El directorio actual es: %s\n", getcwd(buffer,-1));
+
+	if (chdir(dir)!=0) { //Si la funcion chdir devuelve un 0 se cambia el directorio, si es distinto a 0, muestra el error del if.
+		printf("Error al cambiar de directorio: %s\n",strerror(errno));
+		if(line->commands[0].argc>2){// Si el usuario pone 2 argumentos se muestra en pantalla el error
+			fprintf(stderr, "Muchos argumentos.\n");
+		}
 	}
+	printf("El directorio actual es: %s\n",getcwd(ruta,1024));
 }
 
-int main(void) {
-	pid_t pid;
-	char buf[1024];
-	tline * line;
+void Exit() { //Función para salir de la minishell de manera correcta
+	printf("Saliendo de la minishell de manera ordenada.\n");
+	printf("Prompt de la shell: msh>  \n");
+	fin = 1;
+    exit(0);
+}
 
-	while (1) {
+int main() {
+	pid_t pid;
+	char buf[1024]; // Almacena lo que el usuario introduce por la entrada estándar
+	tline * line; // objeto tline para descomponer el buf en comandos
+	signal(SIGINT,SIG_IGN); //Ignora CTRL+C
+	int status; //estado hijo/s
+
+	while(fin == 0){ /*Si la línea introducida no contiene ningún mandato o se ejecuta el mandato en background, 
+		se volverá a mostrar el prompt a la espera de una nueva línea.*/
 		printf("msh> ");
 
-		if (fgets(buf, 1024, stdin) == NULL) {
-            break;
+		if (fgets(buf,1024, stdin) == NULL) { //Por si el usuario pulsa crtl + D
+			break;
         }
 
-		int status;
-		signal(SIGINT,SIG_IGN); //Ignorar Ctrl+C
+		line = tokenize(buf); // Transforma la entrada en un objeto line
 		
-		line = tokenize(buf);
-
 		if (line==NULL) {
 			continue;
 		}
@@ -80,37 +80,38 @@ int main(void) {
 		if (line->background) {
 			printf("comando a ejecutarse en background\n");
 		} 
-		
+
 		if(line->ncommands == 1){ //Si se ejecuta un mandato
-			if (strcmp(line->commands[0].argv[0], "cd") == 0){// Si se ejecuta cd
+			if(strcmp(line->commands[0].argv[0],"exit")==0){ //Si se ejecuta exit
+				Exit();
+			} else if (strcmp(line->commands[0].argv[0], "cd")==0){// Si se ejecuta cd
 				cd(line);
 			}else{
-				pid = fork();
-				if(pid < 0) {  /* Error */
-						fprintf(stderr, "Falló el fork().\n%s\n", strerror(errno));
-						exit(1);
-				}else if(pid == 0){ //  Proceso Hijo
-					if(verificarmandato(line->commands[0].filename)==0){ // comprueba que el mandato es valido
-							execvp(line->commands[0].filename,line->commands[0].argv);
-							//Si llega aquí es que se ha producido un error en el execvp
-							printf("Error al ejecutar el comando: %s\n", strerror(errno));
+				if(verificarmandato(line->commands[0].filename)==0){ // comprueba que el mandato es valido
+					pid=fork();
+					if(pid<0) {  /* Error */
+							fprintf(stderr, "Falló el fork().\n%s\n", strerror(errno));
 							exit(1);
-					}else{ //El mandato no se encuentra
+					}else if(pid==0){ //  Proceso Hijo
+								execvp(line->commands[0].filename,line->commands[0].argv);
+								//Si llega aquí es que se ha producido un error en el execvp
+								printf("Error al ejecutar el comando: %s\n", strerror(errno));
+								exit(1);
+					}else{  /*Proceso padre
+						WIFEXITED(estadoHijo) es 0 si el hijo ha terminado de una manera anormal. 
+						Distinto de 0 si ha terminado porque ha hecho una llamada a la función exit().
+						WEXITSTATUS(estadoHijo) devuelve el valor que ha pasado el hijo a la función exit(), siempre y cuando la 
+						macro anterior indique que la salida ha sido por una llamada a exit(). */
+						wait(&status);
+						if(WIFEXITED(status)!=0)
+						if(WEXITSTATUS(status)!=0)
+							printf("El comando no se ejecutó correctamente\n");
+					}
+				}else{ //El mandato no se encuentra
 						fprintf(stderr,"%s: No se encuentra el mandato\n",line->commands[0].argv[0]);
 					}
-				}else{  /*Proceso padre
-					WIFEXITED(estadoHijo) es 0 si el hijo ha terminado de una manera anormal. 
-					Distinto de 0 si ha terminado porque ha hecho una llamada a la función exit().
-					WEXITSTATUS(estadoHijo) devuelve el valor que ha pasado el hijo a la función exit(), siempre y cuando la 
-					macro anterior indique que la salida ha sido por una llamada a exit(). */
-					wait(&status);
-					if(WIFEXITED(status)!=0)
-					if(WEXITSTATUS(status)!=0)
-						printf("El comando no se ejecutó correctamente\n");
-				}
 			}
-
-		}else if(line->ncommands > 1){ // Si se ejecuta más de 1 mandato
+		}else if(line->ncommands>1){ // Si se ejecuta más de 1 mandato
 			
 		}
 	}
