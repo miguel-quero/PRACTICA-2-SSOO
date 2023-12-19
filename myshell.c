91be2cfde97473fc4ea6465dd08ca8ab0ad9e3e9
quer0 <m.quero.2021@alumnos.urjc.es>
2023-12-19 01:07:11 +0100

    umask  y redirecciones terminadas y comentadas

--- myshell.c
+++ myshell.c
@@ -53,8 +53,63 @@ void siginthandler(){ //Función para tratar CTRL+C
 	fflush(stdout); //Garantiza que los datos del búfer de salida se escriban en salida estándar 
 }
 
+void Umask(tline * line){ //Funcion para ejecutar umask
+	if (line->commands->argc != 2) { //Si la entrada estándar no contiene un número se muestra el siguiente error indicando el uso del mandato
+        fprintf(stderr, "Uso: %s <numero_octal>\n", line->commands->argv[0]);
+    } else { //Si la entrada contiene un número
+		mode_t mask = strtol(line->commands->argv[1],NULL,8); //Se utiliza para convertir un string en un número con una base específica, en este caso 8 (octal)
+    	mode_t maskantigua = umask(mask); //Establece la máscara umask de tipo mode_t pasada por parámetro, y devuelve la máscara anterior
+		printf("La máscara umask anterior era %#o.\n", maskantigua); 
+    	printf("La máscara umask ahora es %#o.\n", mask);
+	}
+}
+
+void redireccionEntrada(char * entrada){ //Función que redirecciona la entrada
+	int fd = open(entrada,O_RDONLY|O_CREAT); //El archivo se abrirá para lectura y si no existe el archivo lo crea
+	if(fd == -1){ //Si se produce un error al abrir el archivo, se muestra el error
+		fprintf(stderr,"%s : Error. %s\n",entrada,strerror(errno)); 
+	}else{ //Si se abrió el archivo de forma correcta, ahora la entrada estándar apunta al archivo
+		dup2(fd,fileno(stdin)); //
+	}	
+}
+
+void redireccionSalida(char * salida){ //Función que redirecciona la salida
+	int fd = creat(salida,S_IRGRP|S_IWOTH|S_IRUSR|S_IWGRP|S_IROTH|S_IWUSR); //Crea el archivo o lo trunca si exite. Las flags utilizadas en resumen son para añadir permisos específicos al grupo, propietario o para otros.
+	if(fd == -1){//Si se produce un error al abrir el archivo, se muestra el error
+		fprintf(stderr,"%s : Error. %s\n",salida,strerror(errno));
+	}else{  //Si se abrió el archivo de forma correcta, ahora la salida estándar apunta al archivo
+		dup2(fd,fileno(stdout)); 
+	}	
+}
+
+void redireccionError(char * error){ //Función que redirecciona la salida para errores
+	int fd = creat(error,S_IWGRP|S_IWOTH|S_IRGRP|S_IRUSR|S_IROTH|S_IWUSR); //Lo mismo que al redireccionar la salida estándar,
+	if(fd == -1){ //Si se produce un error al abrir el archivo, se muestra el error
+		fprintf(stderr,"%s : Error. %s\n",error,strerror(errno)); 
+	}else{ //Si se abrió el archivo de forma correcta, ahora la salida para errores apunta al archivo
+		dup2(fd,fileno(stderr));
+	}	
+}
+
+void reestablecerDescriptores(int entrada, int salida, int error, tline * line){ //Función para reestablecer los descriptores
+	if(line->redirect_input!=NULL){ //Si la entrada es válida se reestablece a la entrada estándar
+		dup2(entrada,fileno(stdin));	
+	}
+	if(line->redirect_error!=NULL){ //Si la salida para el error es válida se reestablece a la salida estándar de error
+		dup2(error,fileno(stderr));	
+	}
+	if(line->redirect_output!=NULL){ //Si la salida es válida se reestablece a la salida estándar
+		dup2(salida,fileno(stdout));	
+	}
+}
+
 int main() {
-	pid_t pid;
+	//Guardamos los descriptores de archivo predeterminados para luego reestablecerlos si se cambian en ejecución
+	int Salida=dup(fileno(stdout));
+	int Entrada=dup(fileno(stdin));
+	int Error=dup(fileno(stderr));
+
+	pid_t pid; //Identificador del proceso actual
 	char buf[1024]; // Almacena lo que el usuario introduce por la entrada estándar
 	tline * line; // objeto tline para descomponer el buf en comandos
 	signal(SIGINT,siginthandler); //Tratar la señal CTRL+C con la funcion siginthandler
@@ -70,24 +125,29 @@ int main() {
 
 		line = tokenize(buf); // Transforma la entrada en un objeto line
 		
-		if (line==NULL) {
+		if (line==NULL){
 			continue;
 		}
-		if (line->redirect_input != NULL) {
+		if (line->redirect_input != NULL){ //Redirecciona la entrada
 			printf("redirección de entrada: %s\n", line->redirect_input);
+			redireccionEntrada(line->redirect_input);
 		}
-		if (line->redirect_output != NULL) {
+		if (line->redirect_output != NULL) {//Redirecciona la salida
 			printf("redirección de salida: %s\n", line->redirect_output);
+			redireccionSalida(line->redirect_output);
 		}
-		if (line->redirect_error != NULL) {
+		if (line->redirect_error != NULL) { //Redirecciona la salida para el error
 			printf("redirección de error: %s\n", line->redirect_error);
+			redireccionError(line->redirect_error);
 		}
 		if (line->background) {
 			printf("comando a ejecutarse en background\n");
 		} 
 
 		if(line->ncommands == 1){ //Si se ejecuta un mandato
-			if(strcmp(line->commands[0].argv[0],"exit")==0){ //Si se ejecuta exit
+			if(strcmp(line->commands[0].argv[0], "umask")==0){ //Si se ejecuta umask
+				Umask(line);
+			} else if(strcmp(line->commands[0].argv[0],"exit")==0){ //Si se ejecuta exit
 				Exit();
 			} else if (strcmp(line->commands[0].argv[0], "cd")==0){// Si se ejecuta cd
 				cd(line);
@@ -119,6 +179,9 @@ int main() {
 		}else if(line->ncommands>1){ // Si se ejecuta más de 1 mandato
 			
 		}
+
+		//Se utiliza para reestablecer los descriptores estándar
+		reestablecerDescriptores(Entrada,Salida,Error,line);
 	}
 	return 0;
 }
